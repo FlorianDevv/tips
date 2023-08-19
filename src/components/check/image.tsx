bafd04b8ce03d124a0c545c8fbd78b3624e053e9
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2023-08-20 00:57:24 +0200

    add placeholder to Images

--- src/components/check/image.tsx
+++ src/components/check/image.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import Image from "next/image";
+
 interface ImageProps {
   image: string;
 }
 
 export default function Images(props: ImageProps) {
   const [sizes, className, content] = props.image.split("|").filter(Boolean);
+  const shimmer = (w: number, h: number) => `
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#333" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`;
 
+  const toBase64 = (str: string) =>
+    typeof window === "undefined"
+      ? Buffer.from(str).toString("base64")
+      : window.btoa(str);
   if (!content || content.trim().length === 0) {
     return null;
   }
@@ -14,6 +32,8 @@ export default function Images(props: ImageProps) {
     <Image
       className={`${className} flex w-auto h-auto`}
       src={content}
+      placeholder="blur"
+      blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
       sizes={sizes}
       width={400}
       height={400}
