414b92f07afd8a8261078299bfea286fbf89d833
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2023-08-22 17:52:39 +0200

    center text

--- src/components/Like/likedTips.tsx
+++ src/components/Like/likedTips.tsx
@@ -35,7 +35,7 @@ export default function LikedTips() {
             />
           ))
         ) : (
-          <p className="text-center text-gray-500 mt-4">No liked tips yet.</p>
+          <p className="mx-auto text-gray-500 mt-4">No liked tips yet.</p>
         )}
       </div>
     </>
