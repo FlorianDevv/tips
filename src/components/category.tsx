205d4bc9a64f22b959663f4a396df0b8bcebf860
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2023-08-25 13:42:09 +0200

    Upgrade category filter with multiple category

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -5,18 +5,17 @@ import data from "../../Data/tipsData.json";
 import Card from "./card";
 
 export default function Category() {
-  const [showMenu, setShowMenu] = React.useState(false);
-  const [selectedCategory, setSelectedCategory] =
-    React.useState("All categories");
+  const [selectedCategories, setSelectedCategories] = React.useState<string[]>(
+    []
+  );
   const [sortOrder, setSortOrder] = React.useState("newest");
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
   const handleCategoryClick = (category: string) => {
-    setSelectedCategory(category);
-    setShowMenu(false);
+    if (selectedCategories.includes(category)) {
+      setSelectedCategories(selectedCategories.filter((c) => c !== category));
+    } else {
+      setSelectedCategories([...selectedCategories, category]);
+    }
   };
 
   const handleSortOrderClick = () => {
@@ -24,10 +23,12 @@ export default function Category() {
   };
 
   const filteredTips =
-    selectedCategory === "All categories"
+    selectedCategories.length === 0
       ? Object.values(data.Tips)
-      : Object.values(data.Tips).filter(
-          (tip: any) => tip.category === selectedCategory
+      : Object.values(data.Tips).filter((tip: any) =>
+          selectedCategories.every((category) =>
+            tip.category.includes(category)
+          )
         );
 
   const sortedTips = [...filteredTips];
@@ -40,12 +41,12 @@ export default function Category() {
     <div className="relative mx-auto">
       <div className="flex flex-col items-center ">
         <div className="flex flex-row items-center ">
-          <button
-            className="bg-gray-800 text-white px-4 py-2 rounded-md"
-            onClick={toggleMenu}
-          >
-            Categories: {selectedCategory}
-          </button>
+          <div className="bg-gray-800 text-white px-4 py-2 rounded-md">
+            Categories:{" "}
+            {selectedCategories.length === 0
+              ? "All categories"
+              : selectedCategories.join(", ")}
+          </div>
           <button
             className="bg-gray-800 text-white px-4 py-2 rounded-md ml-2"
             onClick={handleSortOrderClick}
@@ -53,25 +54,19 @@ export default function Category() {
             {sortOrder === "newest" ? "Sort by oldest" : "Sort by newest"}
           </button>
         </div>
-        {showMenu && (
-          <ul
-            className={`sticky p-2 bg-gray-800 text-white rounded-md shadow-md z-50 mt-2 border border-gray-700 transition-all duration-300 ease-in-out max-w-xs ${
-              showMenu ? "opacity-100 max-h-96" : "opacity-0 max-h-0"
-            }`}
-          >
-            {["All categories", ...data.Category].map((category) => (
-              <li
-                key={category}
-                onClick={() => handleCategoryClick(category)}
-                className={`cursor-pointer py-1 px-2 rounded-md hover:bg-gray-600 ${
-                  selectedCategory === category ? "bg-gray-700" : ""
-                }`}
-              >
-                {category}
-              </li>
-            ))}
-          </ul>
-        )}
+        <ul className="p-2 bg-gray-800 text-white rounded-md shadow-md mt-2 border border-gray-700 transition-all duration-300 ease-in-out max-w-lg grid grid-cols-3 gap-2">
+          {data.Category.map((category) => (
+            <li
+              key={category}
+              onClick={() => handleCategoryClick(category)}
+              className={`cursor-pointer py-1 px-2 rounded-md hover:bg-gray-600 ${
+                selectedCategories.includes(category) ? "bg-gray-700" : ""
+              }`}
+            >
+              {category}
+            </li>
+          ))}
+        </ul>
       </div>
 
       {sortedTips.length > 0 ? (
@@ -88,7 +83,9 @@ export default function Category() {
         </div>
       ) : (
         <p className="text-center text-gray-500 mt-4">
-          No tips found for selected category.
+          {selectedCategories.length === 0
+            ? "No tips found."
+            : "No tips found for selected category."}
         </p>
       )}
     </div>
