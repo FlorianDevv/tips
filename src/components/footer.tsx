25b9f0c4f47626dd9537baa0656ecc36463dbf53
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2023-08-26 22:44:26 +0200

    add year

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <>
       <footer className=" top-0 w-full justify-center p-4 bg-blur bg-gray-400 bg-opacity-10 backdrop-filter backdrop-blur-lg backdrop-saturate-150 shadow-xl ">
-        <div className="">
+        <div className="flex flex-row justify-center">
           <p className="text-2xl text-center text-white">
             Made by{" "}
             <a
@@ -12,8 +14,10 @@ export default function Footer() {
               className="text-blue-500"
             >
               FlorianDevv
+              {"  "}
             </a>
           </p>
+          <p className="text-2xl text-center text-white">©{currentYear}</p>
         </div>
       </footer>
     </>
