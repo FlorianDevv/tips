c85356f05eccffe1ed4bdc4c5acc2216014083fa
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2023-08-27 15:29:45 +0200

    add gestion image and adjust localstorage

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-
 import { useState, useEffect } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const [inputs, setInputs] = useState<string[]>([]);
+  const [imageUrl, setImageUrl] = useState<string>("");
 
   useEffect(() => {
     const storedInputs = JSON.parse(localStorage.getItem("inputs") || "[]");
@@ -28,8 +29,6 @@ export default function Home() {
       newInputs[index - 1] = temp;
       setInputs(newInputs);
       localStorage.setItem("inputs", JSON.stringify(newInputs));
-      localStorage.setItem(`input${index}`, (index - 1).toString());
-      localStorage.setItem(`input${index - 1}`, index.toString());
     }
   };
 
@@ -41,8 +40,6 @@ export default function Home() {
       newInputs[index + 1] = temp;
       setInputs(newInputs);
       localStorage.setItem("inputs", JSON.stringify(newInputs));
-      localStorage.setItem(`input${index}`, (index + 1).toString());
-      localStorage.setItem(`input${index + 1}`, index.toString());
     }
   };
 
@@ -50,15 +47,38 @@ export default function Home() {
     const newInputs = [...inputs, ""];
     setInputs(newInputs);
     localStorage.setItem("inputs", JSON.stringify(newInputs));
-    localStorage.setItem(
-      `input${newInputs.length - 1}`,
-      (newInputs.length - 1).toString()
-    );
+  };
+
+  const handleImageUrlChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const urlPattern = /^(http|https):\/\/[^ "]+$/;
+    const imageUrl = event.target.value;
+    if (urlPattern.test(imageUrl)) {
+      setImageUrl(imageUrl);
+    } else {
+      setImageUrl("");
+    }
+  };
+
+  const isImageUrl = (text: string) => {
+    const urlPattern = /\.(webp)$/i;
+    return urlPattern.test(text);
+  };
+
+  const handleDeleteClick = (index: number) => {
+    const newInputs = [...inputs];
+    newInputs.splice(index, 1);
+    setInputs(newInputs);
+    localStorage.setItem("inputs", JSON.stringify(newInputs));
+  };
+
+  const handleResetAllClick = () => {
+    setInputs([]);
+    localStorage.setItem("inputs", JSON.stringify([]));
   };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      {/* imput to create a title and a image presentation */}
+      {/* input to create a title and an image presentation */}
       <input
         type="text"
         placeholder="Enter title"
@@ -68,13 +88,29 @@ export default function Home() {
         type="text"
         placeholder="Enter image url"
         className="border border-gray-400 rounded px-2 py-1 bg-black mt-2 "
+        value={imageUrl}
+        onChange={handleImageUrlChange}
       />
-
+      {imageUrl && (
+        <Image
+          src={imageUrl}
+          alt="Loaded from URL"
+          className="max-w-full mt-2"
+          width={800}
+          height={800}
+        />
+      )}
       <button
         className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
         onClick={handleButtonClick}
       >
-        Add Text Input
+        Add Text or Image Input
+      </button>
+      <button
+        className="px-4 py-2 text-white bg-red-500 rounded hover:bg-red-600 ml-2"
+        onClick={handleResetAllClick}
+      >
+        Reset All
       </button>
       <div className="mt-4 flex flex-col items-start">
         {inputs.map((input, index) => (
@@ -86,19 +122,35 @@ export default function Home() {
             >
               {"⬆"}
             </button>
-            <input
-              type="text"
-              placeholder="Enter text"
-              value={input}
-              onChange={(event) => handleInputChange(event, index)}
-              className="border border-gray-400 rounded px-2 py-1 bg-black"
-            />
+            {isImageUrl(input) ? (
+              <Image
+                src={input}
+                alt="Loaded from URL"
+                className="max-w-full mt-2"
+                width={800}
+                height={800}
+              />
+            ) : (
+              <input
+                type="text"
+                placeholder="Enter text / image url"
+                value={input}
+                onChange={(event) => handleInputChange(event, index)}
+                className="border border-gray-400 rounded px-2 py-1 bg-black"
+              />
+            )}
             <button
               className="px-2 py-1 text-white bg-green-500 rounded hover:bg-green-600 ml-2"
               onClick={() => handleButtonRightClick(index)}
               disabled={index === inputs.length - 1}
             >
               {"⬇"}
+            </button>
+            <button
+              className="px-2 py-1 text-white bg-red-500 rounded hover:bg-red-600 ml-2"
+              onClick={() => handleDeleteClick(index)}
+            >
+              {"X"}
             </button>
           </div>
         ))}
